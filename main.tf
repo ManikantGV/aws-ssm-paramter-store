90a7fe398b942ac5fa971fd40025624beaaf6725
Manikanta <manikanta.guntika@gmail.com>
2024-02-25 10:52:33 +0530

    Added all components

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "foo" {
   name  = "test.test1"
-  type  = "String"
+  type  = "SecureString"
   value = "Hello World"
+  key_id= "7e4262e5-239e-4f6b-95ab-0bffe6e41f9c"
 }
 
